5a88885553ba25255473c27015cb9194ca075e4e
pippo <pippo@DESKTOP-PR7UVK1>
2024-03-06 16:21:11 +0100

    Aggiungere i file di progetto.

---
+++ Planetario/Pianeti.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using classe_vettori;
+
+namespace Planetario
+{
+    internal class Pianeti
+    {
+        public double Massa { get; set; }
+        public Vettore Spostamento { get; set; }
+        public Vettore Velocita { get; set; }
+        public Brush Brush { get; set; }
+        public Pianeti (double massa, Vettore spostamento, Vettore velocita, Brush brush)
+        {
+            Massa =massa;
+            Spostamento =spostamento;
+            Velocita =velocita;
+            Brush =brush;
+        }
+    }
+}
